b178d8985c208ef7cd95f5bb28bfe7f2449e31e0
Steven Sanderson <spsanderson@gmail.com>
2020-12-18 22:54:35 -0500

    Fixes #10

--- R/diagnostic_plots.R
+++ R/diagnostic_plots.R
@@ -107,6 +107,13 @@ ts_random_walk_ggplot_layers <- function(.data) {
 #' @param .date_col The column holding the timestamp.
 #' @param .value_col The column with the values to be analyzed.
 #' @param .interactive Default is FALSE, TRUE for an interactive plotly plot.
+#' @param .median Default is TRUE. This will show the median line of the data.
+#' @param .cl This is the first upper control line
+#' @param .mcl This is the second sigma control line positive
+#' @param .ucl This is the third sigma control line positive
+#' @param .lc This is the first negative control line
+#' @param .lmcl This is the second sigma negative control line
+#' @param .llcl This si the thrid sigma negative control line
 #'
 #' @examples
 #' library(healthyR.data)
@@ -136,6 +143,7 @@ ts_random_walk_ggplot_layers <- function(.data) {
 #'   ts_qc_run_chart(
 #'     .date_col    = visit_end_date_time
 #'     , .value_col = alos
+#'     , .llcl      = TRUE
 #'   )
 #'
 #' @return
@@ -144,7 +152,9 @@ ts_random_walk_ggplot_layers <- function(.data) {
 #' @export
 #'
 
-ts_qc_run_chart <- function(.data, .date_col, .value_col, .interactive = FALSE) {
+ts_qc_run_chart <- function(.data, .date_col, .value_col, .interactive = FALSE,
+                            .median = TRUE, .cl = TRUE, .mcl = TRUE, .ucl = TRUE,
+                            .lc = FALSE, .lmcl = FALSE, .llcl = FALSE) {
 
     # Tidyeval
     date_col_var_expr  <- rlang::enquo(.date_col)
@@ -179,6 +189,9 @@ ts_qc_run_chart <- function(.data, .date_col, .value_col, .interactive = FALSE) 
     cl_a        <- mean_alos + std_dev
     cl_b        <- mean_alos + (2 * std_dev)
     cl_c        <- mean_alos + (3 * std_dev)
+    cl_d        <- mean_alos - std_dev
+    cl_e        <- mean_alos - (2 * std_dev)
+    cl_f        <- mean_alos - (3 * std_dev)
 
     # Plot
     p <- data_tbl %>%
@@ -189,59 +202,126 @@ ts_qc_run_chart <- function(.data, .date_col, .value_col, .interactive = FALSE) 
             )
         ) +
         ggplot2::geom_line(size = .5, color = "#2C3E50") +
-        ggplot2::geom_line(
-            mapping = ggplot2::aes(y = median_alos)
-            , linetype = "dashed"
-            , size = 1
-            , color = "#6A3D9A"
-        ) +
-        ggplot2::geom_line(
-            mapping = ggplot2::aes(y = cl_a)
-            , color = "#18BC9C"
-            , size = 1
-        ) +
-        ggplot2::geom_line(
-            mapping = ggplot2::aes(y = (mean_alos + (2 * std_dev)))
-            , color = "#CCBE93"
-            , size = 1
-        ) +
-        ggplot2::geom_line(
-            mapping = ggplot2::aes(y = (mean_alos + (3 * std_dev)))
-            , color = "#E31A1C"
-            , size = 1
-        ) +
-        ggplot2::geom_text(
-            mapping = ggplot2::aes(x = max_ds, y = cl_a)
-            , label = round(cl_a, 2)
-            , hjust = -.2
-            , na.rm = TRUE
-        ) +
-        ggplot2::geom_text(
-            mapping = ggplot2::aes(x = max_ds, y = cl_b)
-            , label = round(cl_b, 2)
-            , hjust = -.2
-            , na.rm = TRUE
-        ) +
-        ggplot2::geom_text(
-            mapping = ggplot2::aes(x = max_ds, y = cl_c)
-            , label = round(cl_c, 2)
-            , hjust = -.2
-            , na.rm = TRUE
-        ) +
-        ggplot2::geom_text(
-            mapping = ggplot2::aes(x = max_ds, y = median_alos)
-            , label = median_alos
-            , hjust = -.2
-            , na.rm = TRUE
-        ) +
-        ggplot2::theme_minimal() +
-        ggplot2::labs(
-            caption = "Red Line is the UCL, dashed line is the median"
-        )
+        ggplot2::theme_minimal()
+
+    # Check if add median line
+    if(.median){
+        p <- p +
+            ggplot2::geom_line(
+                mapping = ggplot2::aes(y = median_alos)
+                , linetype = "dashed"
+                , size = 1
+                , color = "#6A3D9A"
+            )
+    }
+
+    # Check if add cl
+    if(.cl){
+        p <- p +
+            ggplot2::geom_line(
+                mapping = ggplot2::aes(y = cl_a)
+                , color = "#18BC9C"
+                , size = 1
+            )
+    }
+
+    # Check if add mcl
+    if(.mcl){
+        p <- p +
+            ggplot2::geom_line(
+                mapping = ggplot2::aes(y = cl_b)
+                , color = "#CCBE93"
+                , size = 1
+            )
+    }
+
+    # Check if add ucl
+    if(.ucl){
+        p <- p +
+            ggplot2::geom_line(
+                mapping = ggplot2::aes(y = cl_c)
+                , color = "#E31A1C"
+                , size = 1
+            )
+    }
+
+    # Check if add lcl
+    if(.lc){
+        p <- p +
+            ggplot2::geom_line(
+                mapping = ggplot2::aes(y = cl_d)
+                , color = "#18BC9C"
+                , size = 1
+            )
+    }
+
+    # Check if add mcl
+    if(.lmcl){
+        p <- p +
+            ggplot2::geom_line(
+                mapping = ggplot2::aes(y = cl_e)
+                , color = "#CCBE93"
+                , size = 1
+            )
+    }
+
+    # Check if add ucl
+    if(.llcl){
+        p <- p +
+            ggplot2::geom_line(
+                mapping = ggplot2::aes(y = cl_f)
+                , color = "#E31A1C"
+                , size = 1
+            )
+    }
 
     # * Interactive ----
     if(.interactive){
-        p <- plotly::ggplotly(p)
+        return(plotly::ggplotly(p))
+    } else {
+        p <- p +
+            ggplot2::geom_text(
+                mapping = ggplot2::aes(x = max_ds, y = cl_a)
+                , label = round(cl_a, 2)
+                , hjust = -.2
+                , na.rm = TRUE
+            ) +
+            ggplot2::geom_text(
+                mapping = ggplot2::aes(x = max_ds, y = cl_b)
+                , label = round(cl_b, 2)
+                , hjust = -.2
+                , na.rm = TRUE
+            ) +
+            ggplot2::geom_text(
+                mapping = ggplot2::aes(x = max_ds, y = cl_c)
+                , label = round(cl_c, 2)
+                , hjust = -.2
+                , na.rm = TRUE
+            ) +
+            ggplot2::geom_text(
+                mapping = ggplot2::aes(x = max_ds, y = cl_d)
+                , label = round(cl_a, 2)
+                , hjust = -.2
+                , na.rm = TRUE
+            ) +
+            ggplot2::geom_text(
+                mapping = ggplot2::aes(x = max_ds, y = cl_e)
+                , label = round(cl_b, 2)
+                , hjust = -.2
+                , na.rm = TRUE
+            ) +
+            ggplot2::geom_text(
+                mapping = ggplot2::aes(x = max_ds, y = cl_f)
+                , label = round(cl_c, 2)
+                , hjust = -.2
+                , na.rm = TRUE
+            ) +
+            ggplot2::geom_text(
+                mapping = ggplot2::aes(x = max_ds, y = median_alos)
+                , label = median_alos
+                , hjust = -.2
+                , na.rm = TRUE
+            )
     }
 
     # * Return ----
