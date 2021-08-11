c032ecd053a18c97b8655f4cf2b5964085b0bef1
Steven Paul Sanderson II, MPH <spsanderson@users.noreply.github.com>
2021-08-10 23:08:57 -0400

    Update calibrate-plot-list.R

--- R/calibrate-plot-list.R
+++ R/calibrate-plot-list.R
@@ -15,12 +15,10 @@
 #'
 #' @examples
 #' suppressPackageStartupMessages(library(modeltime))
-#' suppressPackageStartupMessages(library(healthyR.data))
-#' suppressPackageStartupMessages(library(dplyr))
 #' suppressPackageStartupMessages(library(timetk))
-#' suppressPackageStartupMessages(library(ggplot2))
-#' suppressPackageStartupMessages(library(plotly))
-#' suppressPackageStartupMessages(library(tidyquant))
+#' suppressPackageStartupMessages(library(tidyverse))
+#' suppressPackageStartupMessages(library(healthyR.data))
+#' suppressPackageStartupMessages(library(tidymodels))
 #'
 #' data <- healthyR_data %>%
 #'  filter(ip_op_flag == "I") %>%
@@ -37,6 +35,41 @@
 #'        , .end_date   = "2019"
 #'    )
 #'
+#' splits <- time_series_split(
+#'    data
+#'   , date_col
+#'   , assess = 12
+#'   , skip = 3
+#'   , cumulative = TRUE
+#' )
+#'
+#' rec_obj <- recipe(value ~ ., data = training(splits))
+#'
+#' model_spec <- linear_reg(
+#'    mode = "regression"
+#'    , penalty = 0.1
+#'    , mixture = 0.5
+#' ) %>%
+#'    set_engine("lm")
+#'
+#' wflw <- workflow() %>%
+#'    add_recipe(rec_obj) %>%
+#'    add_model(model_spec) %>%
+#'    fit(training(splits))
+#'
+#' calibrate_and_plot(
+#'   wflw
+#'   , .type = "training"
+#'   , .splits_obj = splits
+#'   , .data = data
+#'  )
+#'
+#' calibrate_and_plot(
+#'   wflw
+#'   , .type        = "testing"
+#'   , .splits_obj = splits
+#'   , .data       = data
+#' )
 #'
 #' @return
 #' The original time series, the simulated values and a some plots
@@ -45,14 +78,13 @@
 
 # *** PLOTTING UTILITY *** ----
 # - Calibrate & Plot
-calibrate_and_plot <- function(..., type = "testing", .splits_obj, .data){
+calibrate_and_plot <- function(..., .type = "testing", .splits_obj, .data){
 
     # Tidyeval ----
-    workflow_var_expr <- rlang::enquos(...)
     splits_obj        <- .splits_obj
 
     # Checks ----
-    if(type == "testing"){
+    if(.type == "testing"){
         new_data = testing(splits_obj)
     } else {
         new_data = training(splits_obj) %>%
@@ -63,19 +95,18 @@ calibrate_and_plot <- function(..., type = "testing", .splits_obj, .data){
         stop(call. = FALSE, "(.data) is missing or is not a data.frame/tibble, please supply.")
     }
 
-    if(!class(splits_obj) == "rsplit") {
+    if(!class(splits_obj)[[1]] == "ts_cv_split") {
+        if(!class(splits_obj)[[2]] == "rsplit") {
+            stop(call. = FALSE, ("(.splits) is missing or is not an rsplit or ts_cv_split. Please supply."))
+        }
         stop(call. = FALSE, ("(.splits) is missing or is not a rsplit or ts_cv_split. Please supply."))
-    }
-
-    if(!class({{{ workflow_var_expr }}}) == "workflow") {
-        stop(call. = FALSE, "Must provide a workflow object")
     }
 
     # Data
     data <- .data
 
     # Calibration Tibble
-    calibration_tbl <- modeltime_table({{{ workflow_var_expr }}}) %>%
+    calibration_tbl <- modeltime_table(...) %>%
         modeltime_calibrate(new_data)
 
     print(calibration_tbl %>% modeltime_accuracy())
@@ -88,4 +119,5 @@ calibrate_and_plot <- function(..., type = "testing", .splits_obj, .data){
         plot_modeltime_forecast(
             .conf_interval_show = FALSE
         )
+
 }
