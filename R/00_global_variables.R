9245de8a786110e962af5e5e8f5ffffe54e2c08e
Steven Paul Sanderson II, MPH <spsanderson@users.noreply.github.com>
2021-08-11 14:30:33 -0400

    Update 00_global_variables.R

--- R/00_global_variables.R
+++ R/00_global_variables.R
@@ -1,6 +1,6 @@
 globalVariables(
     names = c(
         "run","rnorm","x","ds","date_col","value_col","n","as_tibble","value",
-        "index","p50"
+        "index","p50","diff_a","diff_b","ma12"
     )
 )
