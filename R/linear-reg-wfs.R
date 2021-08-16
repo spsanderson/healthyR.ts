f9d3867a2064aa2ae777a5e3d016187d4788ae8c
Steven Paul Sanderson II, MPH <spsanderson@users.noreply.github.com>
2021-08-16 14:24:19 -0400

    Update linear-reg-wfs.R

--- R/linear-reg-wfs.R
+++ R/linear-reg-wfs.R
@@ -12,10 +12,6 @@
 #' defaults set for the `glmnet` model specification, but if you choose you can
 #' set them yourself if you have a good understanding of what they should be.
 #'
-#' @param .data This should be your training data typically obtained from the
-#' splits object training(splits). You can provide a tibble or you can provide
-#' training(splits) as the argument. This will get passed to the
-#' [parsnip::fit()] portion of the [workflows::workflow()]
 #' @param .model_type This is where you will set your engine. It uses
 #' [parsnip::linear_reg()] under the hood and can take one of the following:
 #'   * "lm"
@@ -25,8 +21,9 @@
 #'   * "stan"
 #'   * "spark"
 #'   * "keras"
-#' @param ... The recipes you want to use in the function. You can also place your
-#' recipes in a list as the `...` argument.
+#' @param .recipe_list You must supply a list of recipes. list(rec_1, rec_2, ...)
+#' @param .penalty The penalty parameter of the glmnet. The default is 1
+#' @param .mixture The mixture parameter of the glmnet. The default is 0.5
 #'
 #' @examples
 #' suppressPackageStartupMessages(library(modeltime))
@@ -72,9 +69,38 @@
 #' Returns a workflowsets object.
 #'
 
-linear_reg_wfs <- function(.data, .model_type, ...){
+linear_reg_wfs <- function(.model_type, .recipe_list, .penalty = 1, .mixture = 0.5){
+
+    # * Tidyeval ---
+    model_type  = .model_type
+    recipe_list = .recipe_list
+
+    # * Checks ----
+    if (!is.character(.model_type)) {
+        stop(call. = FALSE, "(.model_type) must be a character like 'lm', 'glmnet'")
+    }
+
+    if (!model_type %in% c("lm","glmnet","all_engines")){
+        stop(call. = FALSE, "(.model_type) must be one of the following, 'lm','glmnet', or 'all_engines'")
+    }
+
+    if (!is.list(recipe_list)){
+        stop(call. = FALSE, "(.recipe_list) must be a list of recipe objects")
+    }
+
+    if (!is.numeric(.penalty) | !is.numeric(.mixture)){
+        stop(call. = FALSE, "Both the .penalty and .mixture parameters must be numeric.")
+    }
+
+    # * Models ----
 
 
+    # * Workflow Sets ----
+
+
+    # * Return ---
+    print(model_type)
+    return(recipe_list)
 
 }
 
