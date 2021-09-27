0cdcd6016bed31ce68b142a6f9a1f9a104f12092
Steven Paul Sanderson II, MPH <spsanderson@users.noreply.github.com>
2021-09-27 11:43:18 -0400

    Update ts-to-tbl.R

--- R/ts-to-tbl.R
+++ R/ts-to-tbl.R
@@ -16,6 +16,8 @@
 #'
 #' @examples
 #'
+#' ts_to_tbl(BJsales)
+#' ts_to_tbl(AirPassengers)
 #'
 #' @return
 #'
@@ -27,23 +29,30 @@ ts_to_tbl <- function(.data){
     ts_obj <- .data
 
     # * Checks ----
-    if(!stats::is.ts(ts_obj) & !xts::is.xts(ts_obj) & !zoo::is.zoo(ts_obj) & !stats::is.mts(ts_obj)){
+    if(!stats::is.ts(ts_obj) & !xts::is.xts(ts_obj) & !zoo::is.zoo(ts_obj) &
+       !stats::is.mts(ts_obj)){
         stop(call. = FALSE, "(.data) needs to be one of ts, xts, mts, or zoo.")
     }
 
     # * Manipulate ----
     ts_tbl <- timetk::tk_tbl(ts_obj, silent = TRUE, timetk_idx = TRUE)
-    %>%
-        dplyr::mutate(
-            date_col = base::paste0(
-                lubridate::year(index),
-                "-",
-                lubridate::month(index),
-                "-01"
+    ts_tbl_names <- base::names(ts_tbl)
+
+    if(!ts_tbl_names[1] == "index"){
+        ts_tbl <- ts_tbl
+    } else {
+        ts_tbl <- ts_tbl %>%
+            dplyr::mutate(
+                date_col = base::paste0(
+                    lubridate::year(index),
+                    "-",
+                    lubridate::month(index),
+                    "-01"
+                ) %>%
+                    lubridate::ymd()
             ) %>%
-                lubridate::ymd()
-        ) %>%
-        dplyr::select(index, date_col, dplyr::everything())
+            dplyr::select(index, date_col, dplyr::everything())
+    }
 
     # * Return ----
     return(ts_tbl)
