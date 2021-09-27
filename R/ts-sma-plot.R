d22a5fb17fc35daecb8382b8effb81cae534eb7b
Steven Paul Sanderson II, MPH <spsanderson@users.noreply.github.com>
2021-09-26 22:34:00 -0400

    Update ts-sma-plot.R

--- R/ts-sma-plot.R
+++ R/ts-sma-plot.R
@@ -11,13 +11,13 @@
 #' moving average work.
 #'
 #' It can only handle a single moving average at a time and therefore if multiple
-#' are called for, it will loop through and append data to a tibble and `ts` object.
+#' are called for, it will loop through and append data to a tibble or `ts` object.
 #'
 #' @param .data The data that you are passing, this can be either a `ts` object or a `tibble`
 #' @param .date_col This only needs to be used if you are passing a `tibble` object.
 #' @param .value_col This only needs to be used if you are passing a `tibble` object.
 #' @param .sma_order This will default to 1. This can be a vector like c(2,4,6,12)
-#' @param .centered This is a bool value of TRUE/FALSE, the default is TRUE
+#' @param .alignment This can be either "left", "center", "right"
 #' @param .partial This is a bool value of TRUE/FALSE, the default is TRUE
 #' @param .multi_plot This is a bool value of TRUE/FALSE, the default is FALSE.
 #' If this is set to TRUE, then all of the moving averages will be put on the plot.
